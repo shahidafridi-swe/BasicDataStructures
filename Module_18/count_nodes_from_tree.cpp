9f07c463bed745d6a5e40ae47b0213143f79fef3
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-01-28 22:23:31 +0600

    Module 18

---
+++ Module_18/count_nodes_from_tree.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+    while (!q.empty())
+    {
+        // 1. ber kore ano
+        Node *p = q.front();
+        q.pop();
+
+        // 2. jabotiyo kaj koro
+        int l, r;
+        cin >> l >> r;
+        Node *left, *right;
+        if (l == -1)
+            left = NULL;
+        else
+            left = new Node(l);
+        if (r == -1)
+            right = NULL;
+        else
+            right = new Node(r);
+
+        p->left = left;
+        p->right = right;
+
+        // 3. children push koro
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+}
+int count_node(Node * root)
+{
+    if(root == NULL ) return 0;
+    int l =count_node(root->left);
+    int r =count_node(root->right);
+    return l+r+1;
+}
+
+
+void levelOrderPrint(Node *root)
+{
+    if(root==NULL) 
+    {
+        cout << " Tree faka";
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        // 1. line the ber kore ana
+        Node *f = q.front();
+        q.pop();
+
+        // 2. jabotiyo sokol kaj kore
+        cout << f->val << " ";
+
+        // 3.children gula k line e deya
+        if (f->left)
+            q.push(f->left);
+        if (f->right)
+            q.push(f->right);
+    }
+}
+
+int main()
+{
+    Node *root = input_tree();
+    levelOrderPrint(root);
+    cout << endl;
+    cout << "The Number of Node " << count_node(root);
+
+    return 0;
+}
---
+++ Module_18/max_height.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+    while (!q.empty())
+    {
+        // 1. ber kore ano
+        Node *p = q.front();
+        q.pop();
+
+        // 2. jabotiyo kaj koro
+        int l, r;
+        cin >> l >> r;
+        Node *left, *right;
+        if (l == -1)
+            left = NULL;
+        else
+            left = new Node(l);
+        if (r == -1)
+            right = NULL;
+        else
+            right = new Node(r);
+
+        p->left = left;
+        p->right = right;
+
+        // 3. children push koro
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+}
+int maximum_height(Node *root)
+{
+    if (root == NULL)
+        return 0;
+    int l = maximum_height(root->left);
+    int r = maximum_height(root->right);
+    return max(l, r) + 1;
+}
+
+int main()
+{
+    Node *root = input_tree();
+
+    cout << "Maximum height" << maximum_height(root);
+
+    return 0;
+}
